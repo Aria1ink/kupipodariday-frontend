b144822e0c1b2b3caa98b46cf5e1f11b7ef3a707
Aria1ink <ann.arialink@yandex.ru>
2024-02-15 18:27:53 +0300

    save

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      key: '~/.ssh/id_ed25519',
+      repo: 'git@github.com:Aria1ink/kupipodariday-frontend.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': 'npm i && npm run build',
+      'post-deploy': `chmod +x ${DEPLOY_PATH}`,
+    },
+  },
+};
